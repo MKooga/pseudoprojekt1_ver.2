eec0522067374d970315ffad1c43f75eba17f149
MKooga <martinkooga@gmail.com>
2015-11-04 13:51:52 +0200

    2. Tere maailm button [Delivers#107385024]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -1,14 +1,19 @@
 <!doctype html>
 <html lang="en">
 <head>
-    
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.3.min.js"></script>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
 <body>
 
+<button onclick="myFunction()">Tere maailm.</button>
 
-
+<script>
+    function myFunction() {
+        alert("Tere maailm!");
+    }
+</script>
 
 </body>
 </html>
