e4c7e7b758ebc02bdd19d12fea79b6e604348c70
MKooga <martinkooga@gmail.com>
2015-11-04 21:58:09 +0200

    5. Kass koeraks [Delivers#107385030]

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -13,6 +13,8 @@
 
 <a href="javascript:linkEi();">jääme siia</a>
 
+<img onclick="Asenda()" src="https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg" id="cat"/>
+
 <script>
     function myFunction() {
         alert("Tere maailm!");
@@ -31,8 +33,20 @@
         alert("jääme siia");
         event.preventDefault()
         window.location="http://www.khk.ee";
-
     }
+</script>
+<script>
+
+    function Asenda() {
+        if (document.getElementById("cat").src == "https://www.petfinder.com/wp-content/uploads/2012/11/140272627-grooming-needs-senior-cat-632x475.jpg")
+        {
+            document.getElementById("cat").src = 'http://bedandbiscuitspetretreat.co.uk/Dog.jpg';
+        }
+        else
+        {
+        }
+    }
+
 </script>
 
 </body>
